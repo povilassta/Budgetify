70d6fc5a29689be9eec3760d7bd786046004b8bf
Povilas Stašys <povilas@gmail.com>
2022-03-02 17:20:11 +0200

    crud for income, expense, categories

--- routes/accounts.js
+++ routes/accounts.js
@@ -27,4 +27,81 @@ accountsRouter.delete("/:id", (req, res) => {
   res.send(`This route deletes user's account with id: ${req.params.id}`);
 });
 
+// INCOMES ----------------------------------------------
+// GET incomes
+accountsRouter.get("/:id/incomes", (req, res) => {
+  res.send(
+    `This route returns incomes for an account with id: ${req.params.id}`
+  );
+});
+
+// POST income
+accountsRouter.post("/:id/incomes", (req, res) => {
+  res.send(
+    `This route creates a new income for an account with id: ${req.params.id}`
+  );
+});
+
+// PATCH income
+accountsRouter.patch("/:id/incomes/:incomeId", (req, res) => {
+  res.send(
+    `This route updates ${req.params.incomeId} income's provided details`
+  );
+});
+
+// DELETE income
+accountsRouter.delete("/:id/incomes/:incomeId", (req, res) => {
+  res.send(`This route deletes ${req.params.incomeId} income`);
+});
+
+// EXPENSES ----------------------------------------------
+// GET expenses
+accountsRouter.get("/:id/expenses", (req, res) => {
+  res.send(
+    `This route returns expenses for an account with id: ${req.params.id}`
+  );
+});
+
+// POST expense
+accountsRouter.post("/:id/expenses", (req, res) => {
+  res.send(
+    `This route creates a new expense for an account with id: ${req.params.id}`
+  );
+});
+
+// PATCH expense
+accountsRouter.patch("/:id/expenses/:expenseId", (req, res) => {
+  res.send(
+    `This route updates ${req.params.expenseId} expense's provided details`
+  );
+});
+
+// DELETE expense
+accountsRouter.delete("/:id/expenses/:expenseId", (req, res) => {
+  res.send(`This route deletes ${req.params.expenseId} expense`);
+});
+
+// CATEGORIES ---------------------------------------------
+// GET categories
+accountsRouter.get("/:id/categories", (req, res) => {
+  res.send(
+    `This route returns expense categories for an account with id: ${req.params.id}`
+  );
+});
+
+// POST category
+accountsRouter.post("/:id/categories", (req, res) => {
+  res.send(`This route creates a category for an account`);
+});
+
+// PATCH category
+accountsRouter.patch("/:id/categories/:categoryId", (req, res) => {
+  res.send(`This route updates category's details`);
+});
+
+// DELETE category
+accountsRouter.delete("/:id/categories/:categoryId", (req, res) => {
+  res.send(`This route removes category`);
+});
+
 export default accountsRouter;
