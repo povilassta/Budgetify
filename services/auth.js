45e5e283384bfb8157527992301fedfb882c17c5
Povilas Stašys <povilas@gmail.com>
2022-03-16 12:03:35 +0200

    refactor authentication to work with db

--- services/auth.js
+++ services/auth.js
@@ -2,8 +2,8 @@ import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 import "dotenv/config";
 import passport from "passport";
 import bcrypt from "bcrypt";
-import db from "../database.js";
 import jwt from "jsonwebtoken";
+import User from "../models/users.js";
 
 // Jwt strategy
 const opts = {
@@ -12,7 +12,7 @@ const opts = {
 };
 
 const jwtStrategy = new JwtStrategy(opts, async (payload, done) => {
-  const user = db.getUserByEmail(payload.email);
+  const user = await User.findOne({ email: payload.email }).exec();
   return done(null, user || false);
 });
 
@@ -25,34 +25,27 @@ export const authJwt = passport.authenticate("jwt", {
 // Login
 export function login(req, res) {
   // Find user in database
-  const user = db.getUserByEmail(req.body.email);
-  // Check if user exists and if the password is correct
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    const payload = {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-    };
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) res.status(500).json({ message: "Something went wrong." });
+    else if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      const payload = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      };
 
-    // Sign the token with payload provided above
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+      // Sign the token with payload provided above
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
 
-    // Send token back
-    res.status(200).json({
-      token: `Bearer ${token}`,
-    });
-  } else {
-    // If user doesn't exist or password is incorrect
-    res.status(401).json({ message: "Invalid credentials" });
-  }
-}
-
-export function register(req, res) {
-  const { email, password, role } = req.body;
-  const user = db.registerUser({ email, password, role });
-  const { password: pass, ...rest } = user;
-
-  res.json(rest);
+      // Send token back
+      res.status(200).json({
+        token: `Bearer ${token}`,
+      });
+    } else {
+      // If user doesn't exist or password is incorrect
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  });
 }
