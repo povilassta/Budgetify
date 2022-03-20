d3b3f48a592c447d1a33bf89512fdc42fbbeff52
Povilas Stašys <povilas@gmail.com>
2022-03-20 23:30:26 +0200

    auth testing

---
+++ tests/auth.test.js
@@ -0,0 +1,47 @@
+import app from "../index.js";
+import supertest from "supertest";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+describe("auth", () => {
+  beforeAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connect(process.env.TESTDB_URL);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  describe("POST /login", () => {
+    describe("When email and password are correct", () => {
+      it("returns jwt token", async () => {
+        const response = await supertest(app)
+          .post("/login")
+          .send({ email: "admin@admin.lt", password: "admin" });
+        expect(response.status).toBe(200);
+        expect(response.body).toMatchObject({
+          token: expect.any(String),
+        });
+        expect(response.header["content-type"]).toBe(
+          "application/json; charset=utf-8"
+        );
+      });
+    });
+
+    describe("When login details are not correct", () => {
+      it("returns error message", async () => {
+        const response = await supertest(app)
+          .post("/login")
+          .send({ email: "wrong@wrong.com", password: "wrong" });
+        expect(response.status).toBe(401);
+        expect(response.body).toMatchObject({
+          message: "Invalid credentials",
+        });
+        expect(response.header["content-type"]).toBe(
+          "application/json; charset=utf-8"
+        );
+      });
+    });
+  });
+});
