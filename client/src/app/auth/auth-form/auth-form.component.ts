ac580a899965055121ba1fdf5c611cb1f359a9c6
Povilas Stašys <povilas@gmail.com>
2022-04-01 09:47:28 +0300

    created authentication form design

---
+++ client/src/app/auth/auth-form/auth-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styleUrls: ['./auth-form.component.sass'],
+})
+export class AuthFormComponent {
+  hide = true;
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password).subscribe((data) => {
+      console.log('successful login');
+      console.log(data);
+      this.router.navigateByUrl('/');
+    });
+  }
+}
