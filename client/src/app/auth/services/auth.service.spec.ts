146ea725f16d78f2e90b6a150d9f00b82f0aecaf
Povilas <povilas1999@gmail.com>
2022-04-17 13:07:08 +0300

    authentication service testing

--- client/src/app/auth/services/auth.service.spec.ts
+++ client/src/app/auth/services/auth.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
     service = TestBed.inject(AuthService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('setSession have to call on success', (done: DoneFn) => {
+    spyOn(service as any, 'setSession');
+    const expectedResult = {
+      token: 'token',
+      expiresIn: '1h',
+      name: 'name',
+    };
+    service.login('email', 'pass').subscribe(() => {
+      expect((service as any).setSession).toHaveBeenCalledOnceWith(
+        expectedResult
+      );
+      done();
+    });
+
+    const req = httpController.expectOne({
+      method: 'POST',
+      url: 'http://localhost:3000/login/',
+    });
+    req.flush(expectedResult);
+  });
+
+  it('setSession have not to call on error', (done: DoneFn) => {
+    spyOn(service as any, 'setSession');
+    service.login('email', 'pass').subscribe({
+      error: () => {
+        expect((service as any).setSession).not.toHaveBeenCalled();
+        done();
+      },
+    });
+
+    const req = httpController.expectOne({
+      method: 'POST',
+      url: 'http://localhost:3000/login/',
+    });
+    req.error(new ProgressEvent('401'));
   });
 });
