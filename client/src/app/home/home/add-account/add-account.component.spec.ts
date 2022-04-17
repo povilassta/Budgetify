cf8fde2b69f8f3a4253791202328000756e9e1c3
Povilas <povilas1999@gmail.com>
2022-04-17 13:06:53 +0300

    fix or delete some of the basic generated tests

--- client/src/app/home/home/add-account/add-account.component.spec.ts
+++ client/src/app/home/home/add-account/add-account.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AddAccountComponent } from './add-account.component';
 
@@ -8,9 +10,9 @@ describe('AddAccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddAccountComponent ]
-    })
-    .compileComponents();
+      declarations: [AddAccountComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
